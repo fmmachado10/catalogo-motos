18db494140ef7b897a8284e5532e22071a779dd2
fmmachado10 <fabricio.machado@tcm.go.gov.br>
2019-11-28 18:40:28 -0300

    Adicionando Melhorias

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,17 +4,12 @@ import { Routes, RouterModule } from '@angular/router';
 import {CatMotosComponent} from './cat-motos/cat-motos.component';
 import {HomeComponent} from './home/home.component';
 import {HttpClientModule} from '@angular/common/http';
-import {CatRacaComponent} from './cat-raca/cat-raca.component';
 
 const routes: Routes = [
 
   {
     path: 'cadastroMotoForm',
     component: CatMotosComponent
-  },
-  {
-    path: 'cadastroRacaForm',
-    component: CatRacaComponent
   },
   {
     path: '',
